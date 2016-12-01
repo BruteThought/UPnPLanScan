d9b909639fa5d1ce6a7cd22b199690380cd00cb1
BruteThought <calvinbrierley@gmail.com>
2016-12-01 02:31:36 +0000

    REGEX/Scanning complete. Now times out and prints the discovered devices

---
+++ mSearch.py
@@ -0,0 +1,22 @@
+class mSearch:
+    def __init__(self, cache, date, location, opt, nls, server, userAgent, st, usn):
+        self.cache = cache
+        self.date = date
+        self.location = location
+        self.opt = opt
+        self.nls = nls
+        self.server = server
+        self.userAgent = userAgent
+        self.st = st
+        self.usn = usn
+
+    def printinfo(self):
+        print("Cache: "+ self.cache)
+        print("Date: " + self.date)
+        print("Location: " + self.location)
+        print("Opt: " + self.opt)
+        print("NLS: " + self.nls)
+        print("Server: " + self.server)
+        print("User Agent: " + self.userAgent)
+        print("ST: " + self.st)
+        print("USN: " + self.usn)
--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 import socket
 import struct
 import codecs
+import re
+import time
+from mSearch import *
+
 
 UDP_IP = "239.255.255.250"
 UDP_PORT = 1900
-
+MX = 10
 MESSAGE = "M-SEARCH * HTTP/1.1\r\n" \
           "HOST:239.255.255.250:1900\r\n" \
           "ST:upnp:rootdevice\r\n" \
-          "MX:10\r\n" \
+          "MX:" + str(MX) + "\r\n" \
           "MAN:\"ssdp:discover\"\r\n\r\n"
+
+devicedict = {}
+receiving = 1
+
+def decodepacket(packet):
+        cache = cleanReg(re.search(r'(?:CACHE-CONTROL: ?)(.*)', packet))
+        date = cleanReg(re.search(r'(?:DATE: ?)(.*)', packet))
+        location = cleanReg(re.search(r'(?:LOCATION: ?)(.*)', packet))
+        opt = cleanReg(re.search(r'(?:OPT: ?)(.*)', packet))
+        nls = cleanReg(re.search(r'(?:NLS: ?)(.*)', packet))
+        server = cleanReg(re.search(r'(?:SERVER: ?)(.*)', packet))
+        userAgent = cleanReg(re.search(r'(?:X-User-Agent: ?)(.*)', packet))
+        st = cleanReg(re.search(r'(?:ST: ?)(.*)', packet))
+        usn = cleanReg(re.search(r'(?:USN: ?)(.*)', packet))
+
+        device = mSearch(cache, date, location, opt, nls, server, userAgent, st, usn)
+        return device
+
+def cleanReg(result):
+    if result:
+        return result.group(1)
+    else:
+        return ""
+
+
+
+
 
 # Set up the UDP port, bind it, then send the M-SEARCH packet.
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -37,6 +68,34 @@ mreq = struct.pack("4sl", socket.inet_aton(UDP_IP), socket.INADDR_ANY)
 # mreq: set it to thea multicast address on all interfaces.
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-while True:
-    test = str(sock.recv(10240),'utf-8')
-    print(codecs.getdecoder("unicode_escape")(test)[0])
+
+# Start listening for responses
+# timeout 5 seconds after the required response time to have a look at devices
+timeout = time.time()+ MX+5
+while receiving:
+    message = str(sock.recv(10240),'utf-8')
+    # If the protocol matches
+    protocol = re.match(r'^.*', message)
+    if protocol.group(0).strip() == "HTTP/1.1 200 OK":
+
+        # Comb through the packet info and put it in a python object
+
+        packet = decodepacket(codecs.getdecoder("unicode_escape")(message)[0])
+
+        # Check the USN and put it into an array if there are no matches.
+        if not packet.usn in devicedict:
+            print("found device")
+            devicedict[packet.usn] = packet
+
+    else:
+        # Not the right protocol, discard
+        print("not correct protocol")
+
+    # If sufficient time has passed, break out of it.
+    if time.time() > timeout:
+        break
+print("---FINISHED SCANNING---")
+for key in devicedict:
+    print("----START DEVICE INFO----")
+    devicedict[key].printinfo()
+    print("----END DEVICE INFO----")
