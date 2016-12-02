75bbb31aaf5748aa72eca770e1fadb8f4cd20ccb
BruteThought <calvinbrierley@gmail.com>
2016-12-02 06:42:11 +0000

    XML can now get XML documents remotely and parse them as a string

--- XMLReader.py
+++ XMLReader.py
@@ -1,14 +1,15 @@
 # TODO: try/catch for missing tags/files
 import re
-import urllib
+import urllib.request
 import xml.etree.ElementTree as ET
 from service import service
 
 
 def getServices(XMLURL):
+    print("--Attemtping to open remote XML document---")
     XMLDocument = urllib.request.urlopen(XMLURL).read()
-    tree = ET.parse(XMLDocument)
-    root = tree.getroot()
+    print("---Document received---")
+    root = ET.fromstring(XMLDocument)
     XMLNamespace = re.match('\{.*\}', root.tag).group(0)
     serviceArray = []
 
@@ -16,6 +17,7 @@ def getServices(XMLURL):
     device = root.find(XMLNamespace + 'device')
     servicelist = device.find(XMLNamespace + 'serviceList')
     for serviceNode in servicelist.findall(XMLNamespace + 'service'):
+
         serviceType = serviceNode.find(XMLNamespace + 'serviceType').text
         serviceId = serviceNode.find(XMLNamespace + 'serviceId').text
         copntrolURL = serviceNode.find(XMLNamespace + 'controlURL').text
